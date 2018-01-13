b7c116421f0da3b774dae06ba19961483a977884
Justin Daniel <justinddaniel@gmail.com>
2018-01-13 01:34:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function distanceFromHqInBlocks (blocks) {
 function distanceFromHqInFeet (blocks) {
    return distanceFromHqInBlocks (blocks) * 264
 }
+
+function distanceTravelledInFeet (origin, destination) {
+  return Math.abs(origin - destination) * 264
+}
